1a477d0a0b56d8e359157dcb54a92c1ffca79424
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-18 16:15:28 +0700

    fix: LoginController

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
@@ -13,5 +14,21 @@ class LoginController extends Controller
     public function __invoke(Request $request)
     {
         //
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = auth()->guard('api')->attempt($credentials)) {
+            return response()->json(['success' => false, 'message' => 'Incorrect Email or Password'], 401);
+        }
+
+        return response()->json(['success' => true, 'user' => auth()->guard('api')->user(), 'token' => $token]);
     }
 }
