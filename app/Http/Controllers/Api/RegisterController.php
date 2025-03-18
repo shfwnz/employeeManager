295b8f62c6caafeb11b2427b3f156f873beb52f1
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-18 15:58:30 +0700

    fix: regist user through api

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use function Laravel\Prompts\password;
 
 class RegisterController extends Controller
 {
@@ -12,6 +16,29 @@ class RegisterController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name'      => 'required',
+            'email'     => 'required|email|unique:users',
+            'password'  => 'required|min:8|confirmed'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcsqrt($request->password)
+        ]);
+
+        if ($user) {
+            return response()->json([
+                'success' => true,
+                'user' => $user
+            ], 201);
+        }
+
+        return response()->json(['success' => false], 409);
     }
 }
---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
