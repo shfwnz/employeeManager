3044319806f3b5916cbd3d99194883d3a03b98fc
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-18 15:39:02 +0700

    add: jwt conf

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -44,5 +45,16 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // Jwt Conf
+    public function getJWTIdentifier()
+    {
+        return $this->get_key();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
