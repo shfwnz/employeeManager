5617614e7f84590d6bb6f79307fc7e84094cb46b
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-19 13:46:08 +0700

    fix: post employee

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -6,13 +6,50 @@ use App\Models\Employee;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EmployeeResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class EmployeeController extends Controller
 {
     public function index()
     {
-        $employee = Employee::latest()->paginate(5);
+        $employees = Employee::latest()->paginate(5);
 
-        return new EmployeeResource(true, 'list employee', $employee);
+        return new EmployeeResource(true, 'list employee', $employees);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nik' => 'required|unique:karyawan,nik',
+            'nama_lengkap' => 'required|string|max:255',
+            'tempat_lahir' => 'required|string|max:255',
+            'tanggal_lahir' => 'required|date',
+            'jenis_kelamin' => 'required|in:Laki-laki,Perempuan',
+            'alamat' => 'required|string',
+            'telepon' => 'required|string|max:15',
+            'email' => 'required|email|unique:karyawan,email',
+            'foto' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
+            'status' => 'required|in:Aktif,Nonaktif'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $employee = Employee::create([
+            'nik' => $request->nik,
+            'nama_lengkap' => $request->nama_lengkap,
+            'tempat_lahir' => $request->tempat_lahir,
+            'tanggal_lahir' => $request->tanggal_lahir,
+            'jenis_kelamin' => $request->jenis_kelamin,
+            'alamat' => $request->alamat,
+            'telepon' => $request->telepon,
+            'email' => $request->email,
+            'foto' => $request->foto,
+            'status' => $request->status ?? 'Aktif'
+        ]);
+
+        return new EmployeeResource(true, 'Data employee ditambahkan', $employee);
     }
 }
