f193eddd010230ce1ed1adb1c8fdb476a566eae8
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-19 16:47:35 +0700

    fix: position model

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -2,14 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Position extends Model
 {
+    use HasFactory;
+
     protected $table = 'jabatan';
 
     protected $fillable = [
         'nama_jabatan',
         'deskripsi'
     ];
+
+    public function jobs()
+    {
+        return $this->hasMany(Job::class, 'jabatan_id');
+    }
 }
