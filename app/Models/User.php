b798fc98bf5c672a7790924b31b7c997191c5a9c
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-25 07:31:39 +0700

    docs: comment for better understanding

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,35 +9,22 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements JWTSubject
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    // Fillable
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    // Hidden attributes
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    // conversion
     protected function casts(): array
     {
         return [
@@ -47,12 +33,13 @@ class User extends Authenticatable implements JWTSubject
         ];
     }
 
-    // Jwt Conf
+    // JWT identifier
     public function getJWTIdentifier()
     {
         return $this->getKey();
     }
 
+    // JWT custom claims
     public function getJWTCustomClaims()
     {
         return [];
