baa9f3700a427c345fc0e3e0b1013766e5ef0a9d
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-25 07:17:07 +0700

    docs: comment for better understanding

--- app/Http/Resources/BaseResource.php
+++ app/Http/Resources/BaseResource.php
@@ -7,17 +7,27 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class BaseResource extends JsonResource
 {
+    // response status
     public $status;
+
+    // response message
     public $message;
+
+    // response data
     public $resource;
 
+    // constructor method
     public function __construct($status, $message, $resource)
     {
+        // call parent constructor
         parent::__construct($resource);
+
+        // set response attributes
         $this->status = $status;
         $this->message = $message;
     }
 
+    // format response
     public function toArray(Request $request): array
     {
         return [
