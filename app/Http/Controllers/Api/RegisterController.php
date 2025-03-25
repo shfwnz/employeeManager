03f834566ac1a64759a99f07366d1f9902e96e3e
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-25 07:16:36 +0700

    docs: comment for better understanding

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -8,31 +8,32 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 
-use function Laravel\Prompts\password;
+// use function Laravel\Prompts\password;
 
 class RegisterController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
+        // validate request
         $validator = Validator::make($request->all(), [
             'name'      => 'required',
             'email'     => 'required|email|unique:users',
             'password'  => 'required|string|min:8|confirmed'
         ]);
 
+        // validation failed?
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
+        // create new user
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
 
+        // user created?
         if ($user) {
             return response()->json([
                 'success' => true,
@@ -40,6 +41,7 @@ class RegisterController extends Controller
             ], 201);
         }
 
+        // registration failed
         return response()->json(['success' => false], 409);
     }
 }
