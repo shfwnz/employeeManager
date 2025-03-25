3e62a7ed4e01f572685de87bb43d88f35c6c6977
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-25 07:24:44 +0700

    docs: comment for better understanding

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -9,8 +9,10 @@ class Employee extends Model
 {
     use HasFactory;
 
+    // Table name
     protected $table = 'karyawan';
 
+    // Fillable 
     protected $fillable = [
         'nik',
         'nama_lengkap',
@@ -24,16 +26,19 @@ class Employee extends Model
         'status',
     ];
 
+    // One-to-Many
     public function jobs()
     {
         return $this->hasMany(Job::class, 'karyawan_id');
     }
 
+    // One-to-One
     public function division()
     {
         return $this->hasOneThrough(Division::class, Job::class, 'karyawan_id', 'id', 'id', 'divisi_id');
     }
 
+    // One-to-One
     public function position()
     {
         return $this->hasOneThrough(Position::class, Job::class, 'karyawan_id', 'id', 'id', 'jabatan_id');
